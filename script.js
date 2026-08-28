e2669c5bf29959f48c658ecc9b1e0b1492b2588f
chormchhaiya268-source <chormchhaiya268@gmail.com>
2026-08-28 13:47:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2332,5 +2332,5 @@ window.startAttendanceFlow = async function () {
 // after student agrees to consent.
 // ================================================
 const savedLang =
-  localStorage.getItem("studentLanguage") || "en";
+  localStorage.getItem("studentLanguage") || "km";
 applyLanguage(savedLang);
